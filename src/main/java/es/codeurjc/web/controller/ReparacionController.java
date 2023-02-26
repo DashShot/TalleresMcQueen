a89d95f32e6b8afdefc4d57b8d247bf4555ee949
DavidRG25 <davidrodriguezg25@gmail.com>
2023-02-26 19:28:55 +0100

    26/02/2023/19:28

--- src/main/java/es/codeurjc/web/controller/ReparacionController.java
+++ src/main/java/es/codeurjc/web/controller/ReparacionController.java
@@ -1,48 +1,75 @@
 package es.codeurjc.web.controller;
 
+import java.io.IOException;
+import java.net.http.HttpHeaders;
 import java.sql.SQLException;
 import java.util.Optional;
 
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mysql.cj.jdbc.Blob;
 
 import es.codeurjc.web.models.Reparacion;
 import es.codeurjc.web.service.ReparacionService;
 
 @Controller
+@RequestMapping("/reparaciones")
 public class ReparacionController {
     
   @Autowired
   private ReparacionService reparacionService;
 
-  @GetMapping("/Reparaciones")
+  @GetMapping
   public String reparaciones(Model model){
-      model.addAttribute("Reparaciones", reparacionService.findAll());
-      return "Reparaciones";
+      model.addAttribute("reparaciones", reparacionService.findAll());
+      return "reparaciones";
   }
 
-  @GetMapping("/Reparaciones/{id}")
-  public String verReparacion(Model model, @PathParam long id){
-    Optional<Reparacion> Reparacion = reparacionService.findbyID(id);
-    if (Reparacion.isPresent()){
-        model.addAttribute("Reparacion", Reparacion.get());
-        return "Reparacion";
+  @GetMapping("/{id}")
+  public String verReparacion(Model model, @PathVariable long id){
+    Optional<Reparacion> reparacion = reparacionService.findbyID(id);
+    if (reparacion.isPresent()){
+        model.addAttribute("reparacion", reparacion.get());
+        return "reparacion";
     }
-    return "Reparaciones";
+    return "reparaciones";
   }
+  @GetMapping("/nueva_reparacion")
+    public String nuevaReparacion(Model model) {
+        model.addAttribute("reparacion", new Reparacion());
+        return "nueva_reparacion";
+    }
 
-  @GetMapping("/Reparaciones/{id}/imagenes")
-  public ResponseEntity<Object> verImagen(@PathVariable Long id) throws SQLException{
+    @PostMapping("/guardar_reparacion")
+    public String guardarReparacion(@ModelAttribute Reparacion reparacion) {
+        reparacionService.save(reparacion);
+        return "redirect:/reparaciones";
+    }
+  /**
+   * @param id
+   * @return
+   * @throws IOException
+   */
+  @GetMapping("/{id}/imagenes")
+  public ResponseEntity<byte[]> verImagen(@PathVariable Long id) throws IOException {
 
-      Optional<Reparacion> Reparacion = reparacionService.findbyID(id);
-      if (Reparacion.isPresent() && Reparacion.get().getImagenIni()){
-        
+      Optional<Reparacion> reparacion = reparacionService.findbyID(id);
+      if (reparacion.isPresent() && reparacion.get().getImagenIni() != null){
+        Blob imagen = reparacion.get().getImagenIni();
+        HttpHeaders headers = new HttpHeaders(null);
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentLength(imagen.length);
+        return new ResponseEntity<>(imagen, headers, HttpStatus.OK);
       }
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
   }
 }
