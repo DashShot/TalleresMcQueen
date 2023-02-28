fdb2f1d49ae0abc412917b524c1f34969f5e9c30
DashShot <susiloko33@gmail.com>
2023-02-28 13:36:16 +0100

    Imagen inicio conseguida 28/02 13:35

--- src/main/java/es/codeurjc/web/repository/UsuarioRepository.java
+++ src/main/java/es/codeurjc/web/repository/UsuarioRepository.java
@@ -1,10 +1,15 @@
 package es.codeurjc.web.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import es.codeurjc.web.models.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+   // @Query("select u from User u where UPPER(u.nombre) LIKE UPPER(CONCAT ('%',?1,'%'))")
+	//List<Usuario> findByUsuario(String texto);
     
     Usuario getByNombre(String nombre);
     
